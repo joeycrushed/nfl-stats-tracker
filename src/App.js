ab0c44e7faf0edbcb32cf4ef057a01001e70b219
Joe Smith <joeycrushed@gmail.com>
2021-09-14 18:13:06 +0100

    Add React Router & Team Stats Screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import TeamCard from './TeamCard';
+import TeamStatsScreen from './screens/TeamStatsScreen';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-    <h1>NFL Team Stats</h1>
-      <div class="teamsContainer">
-        <TeamCard />
-      </div>
-    </div>
-  )
-}
+    <Router>
+      <Switch>
 
-export default App;
+        <Route path="/teamstatistics">
+          <TeamStatsScreen />
+        </Route>
+
+        <Route path="/">
+          <div className="App">
+          <h1>NFL Team Stats</h1>
+          <div class="teamsContainer"></div>
+            <TeamCard/>
+          </div>
+        </Route>
+
+      </Switch>
+    </Router>
+    
+    
+    
+    
+    
+    )
+  }
+  
+  export default App;
+  
---
+++ src/screens/TeamStatsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TeamStatsScreen.css'
+
+function TeamStatsScreen() {
+    return (
+        <div>
+            <h1>Team Stats Screen</h1>
+        </div>
+    )
+}
+
+export default TeamStatsScreen
