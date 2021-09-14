2a01f7929635b16caa9e5cceb3ec6280214b94cc
Joe Smith <joeycrushed@gmail.com>
2021-09-14 16:48:08 +0100

    map through team names

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+  const url = "https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4391"
+  const [team, setTeam] = useState(null)
+
+useEffect(() => {
+  axios.get(url)
+  .then(response => {
+    setTeam(response.data)
+  })
+}, [url])
+
+if (team) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>NFL Teams</h1>
+      <ul>
+        {team.teams.map(nfl =>
+          <li>{nfl.strTeam}</li>
+        )}
+      </ul>
+    </div>
+  
+  )
+}
+
+  return (
+    <div >  
+    <p>it's not working</p>
     </div>
   );
 }
---
+++ src/Request.js
@@ -0,0 +1,5 @@
+const requests = {
+    allTeams: "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=NFL"
+}
+
+export default requests
